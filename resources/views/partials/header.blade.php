9d87bc1d8c3ef5c67f05e2376f2f7bf366f7288f
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-07-01 18:00:55 +0200

    Added: active class

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header>
     <nav>
-        <a href={{ route("section_home") }}>HOME</a>
-        <a href={{ route("section_about") }}>ABOUT</a>
+        <a class="@if (Route::currentRouteName() == 'section_home') active @endif" href="{{ route('section_home') }}">HOME</a>
+        <a class="@if (Route::currentRouteName() == 'section_about') active @endif" href="{{ route('section_about') }}">ABOUT</a>
     </nav>
-    
 </header>
+
+{{-- Uso la classe Active sul link che corrisponde alla view in cui mi trovo --}}
