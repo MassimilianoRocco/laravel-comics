1aea1e9e9374bd300711a21d7005e5809d8ad60c
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-07-02 10:21:50 +0200

    Added: general css for layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', "Laravel Template")</title>
+    <link rel="stylesheet" href="/app.css">
     @yield('styles')
+    
 </head>
 
 <body>
